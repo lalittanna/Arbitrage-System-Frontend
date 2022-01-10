5b95f95e214984b7bacb04bfbd251b74e22a84cb
lalit <lalittanna77@gmail.com>
2022-01-10 20:34:33 +0000

    Increased navbar width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <ThemeProvider theme={themeMode}>
       <nav className="w-full flex md:justify-center justify-between items-center p-4">
-        <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <div className="md:flex-[0.8] flex-initial justify-center items-center">
           <img src={logo} alt="logo" className="w-32 cursor-pointer" />
         </div>
         <GlobalStyles />
