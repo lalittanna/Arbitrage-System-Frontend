2c5539ac5749f984d84f7cd5b31b05f2f2e4a68c
lalit <lalittanna77@gmail.com>
2022-01-10 20:06:28 +0000

    Added styles for darkmode

---
+++ src/styles/globalStyles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    body{
+        background: ${({ theme }) => theme.body};
+        color: ${({ theme }) => theme.text};
+        transition: all .5s linear;
+    }
+`;
+
+export const lightTheme = {
+  body: "#fff",
+  text: "#121212",
+};
+
+export const darkTheme = {
+  body: "#121212",
+  text: "#fff",
+};
