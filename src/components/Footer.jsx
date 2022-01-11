e51efeb6c748ceb492d21a6a296484aef817d433
lalit <lalittanna77@gmail.com>
2022-01-11 22:41:52 +0000

    Added <hr/>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 bg-[#00ffd4] absolute bottom-0 left-0 right-0">
-      <hr />
+      <div className="sm:w-[50%] w-full h-[0.25px] bg-gray-400 mt-7" />
       <div>
-        <p className="text-black mt-5 text-center font-normal text-base">
+        <p className="text-black text-center font-normal text-base">
           &copy;{new Date().getFullYear()} Aata the Dough dam raeet INC.
         </p>
       </div>
