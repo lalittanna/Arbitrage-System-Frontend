e973822bde196de517c23e93d6dbc9b4e2b956dc
lalit <lalittanna77@gmail.com>
2022-01-12 18:43:00 +0000

    Added redirecting link to github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ import { BsGithub } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 bg-[#00ffd4] absolute bottom-0 left-0 right-0">
-      <BsGithub fontSize={28} className="text-black cursor-pointer" />
+      <BsGithub
+        fontSize={28}
+        className="text-black cursor-pointer"
+        onClick={() =>
+          window.open(
+            "https://github.com/lalittanna/Arbitrage-System-Frontend",
+            "_blank"
+          )
+        }
+      />
       <div className="sm:w-[50%] w-full h-[0.25px] bg-gray-400 mt-7" />
       <div>
         <p className="text-black text-center font-normal text-base">
